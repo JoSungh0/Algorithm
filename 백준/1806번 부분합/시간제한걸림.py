d9a17d5c735ecf15e7305b5fb108d138c3db92a2
JoSungh0 <132130357+JoSungh0@users.noreply.github.com>
2024-03-12 23:56:44 +0900

    Update 시간제한걸림.py

--- 백준/1806번 부분합/시간제한걸림.py
+++ 백준/1806번 부분합/시간제한걸림.py
@@ -2,21 +2,21 @@ import sys
 
 N, S = map(int, sys.stdin.readline().split())
 
-seqeunce = list(map(int, sys.stdin.readline().split()))
-if len(seqeunce) != N:
+sequence = list(map(int, sys.stdin.readline().split()))
+if len(sequence) != N:
     print("false")
 first = True
 for n in range(N):
-    i = len(seqeunce) - n
+    i = len(sequence) - n
     for j in range(i):
         sum = 0
         for k in range(j, j+n):
-            sum += seqeunce[k]
+            sum += sequence[k]
         if sum >= S:
             first = False
             print(n)
             break
     if first == False:
         break
-if n == len(seqeunce):
+if n == len(sequence):
     print(0)
